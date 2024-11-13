d4c84226a2aff16e4a78685d88e67ac7d5fd30c7
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-11-13 09:18:12 -0500

    fix example

--- test/example.f90
+++ test/example.f90
@@ -1,9 +1,10 @@
 program example
   use, intrinsic :: iso_c_binding
-  use :: directory
+  use :: files_f90
   implicit none
 
   type(directory_reader) :: reader
+  type(file_reader) :: file_read
   integer(c_int) :: i
 
   ! This will read this project's own directory and list the files/folders.
@@ -18,8 +19,20 @@ program example
 
   print*,"== FILES =="
 
+  ! To string.
   do i = 1,reader%file_count
     print*,reader%files(i)
+    call file_read%read_file(reader%files(i)%string)
+    print*,file_read%file_string
+    call file_read%destroy()
+  end do
+
+  ! To lines.
+  do i = 1,reader%file_count
+    print*,reader%files(i)
+    call file_read%read_lines(reader%files(i)%string)
+    print*,file_read%lines
+    call file_read%destroy()
   end do
 
   ! Remember to close it.
