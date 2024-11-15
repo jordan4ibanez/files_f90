e87722af5c8bdc8fa44b515fb8ea3883d4c142ec
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-11-15 01:58:17 -0500

    update tutorial

--- test/example.f90
+++ test/example.f90
@@ -3,26 +3,28 @@ program example
   use :: files_f90
   implicit none
 
-  type(directory_reader) :: reader
+  type(directory_reader) :: dir_reader
   type(file_reader) :: file_read
   integer(c_int) :: i
 
   ! This will read this project's own directory and list the files/folders.
 
-  call reader%read_directory("./")
+  if (.not. dir_reader%read_directory("./")) then
+    error stop
+  end if
 
   print*,"== FOLDERS =="
 
-  do i = 1,reader%folder_count
-    print*,reader%folders(i)
+  do i = 1,dir_reader%folder_count
+    print*,dir_reader%folders(i)
   end do
 
   print*,"== FILES =="
 
   ! To string.
-  do i = 1,reader%file_count
-    print*,reader%files(i)
-    if (.not. file_read%read_file(reader%files(i)%string)) then
+  do i = 1,dir_reader%file_count
+    print*,dir_reader%files(i)
+    if (.not. file_read%read_file(dir_reader%files(i)%string)) then
       error stop
     end if
     print*,file_read%file_string
@@ -30,9 +32,9 @@ program example
   end do
 
   ! To lines.
-  do i = 1,reader%file_count
-    print*,reader%files(i)
-    if (.not. file_read%read_lines(reader%files(i)%string)) then
+  do i = 1,dir_reader%file_count
+    print*,dir_reader%files(i)
+    if (.not. file_read%read_lines(dir_reader%files(i)%string)) then
       error stop
     end if
     print*,file_read%lines
@@ -40,6 +42,6 @@ program example
   end do
 
   ! Remember to close it.
-  call reader%destroy()
+  call dir_reader%destroy()
 
 end program example
